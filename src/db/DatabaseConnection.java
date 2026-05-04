74671f8307b36a67bbf2331da4eb55de78ee7693
mariammkkk <mariamkhan8805@gmail.com>
2026-05-03 22:12:02 -0400

    ensure single connection

--- src/db/DatabaseConnection.java
+++ src/db/DatabaseConnection.java
@@ -4,19 +4,23 @@ import java.sql.DriverManager;
 
 public class DatabaseConnection {
 
-    public static Connection getConnection() {
-        try {
-            System.out.println("Database Connected...");
-            return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/travel_reservation",
-                "root",
-                "..."
-            );
+    private static Connection con = null; // ensures only one connection is made instead of a new db connection every time
 
-        } catch (Exception e) {
-            System.out.println("ERROR: Database not Connected");
-            e.printStackTrace();
-            return null;
+    public static Connection getConnection() {
+        if (con == null) { // if connection is null, then create a new connection, otherwise return the existing connection
+            try {
+                System.out.println("Database Connected...");
+                return DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/travel_reservation",
+                    "root",
+                    "..."
+                );
+
+            } catch (Exception e) {
+                System.out.println("ERROR: Database not Connected");
+                e.printStackTrace();
+            }
         }
+        return con;
     }
 }
